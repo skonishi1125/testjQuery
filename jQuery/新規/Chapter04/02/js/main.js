518a30f5bed0bd3e5c854508ed9a7e0fedb72d96
skonishi1125 <skonishi1125@gmail.com>
2021-01-20 08:57:43 +0900

    chapter4終了まで

--- jQuery/新規/Chapter04/02/js/main.js
+++ jQuery/新規/Chapter04/02/js/main.js
@@ -3,39 +3,76 @@ $(function(){
     var duration = 300;
 
     // images ----------------------------------------
+    // jQueryオブジェクトを格納する 格納していることがわかりやすいように$をつける
     var $images = $('#images p');
 
     // images 1つ目の画像
+    // filter:jQueryオブジェクトの要素から、さらに絞り込むメソッド findとの違いはP113
+    // 
+    // id="images"の中のpの、1番目の要素にマウスが乗った時、乗った要素のstrongとspanに処理をする
+    // 
+
+    // $('#images p:nth-child(1)')でも動作した
     $images.filter(':nth-child(1)')
-        .on('mouseover', function(){
-            $(this).find('strong, span').stop(true).animate({opacity: 1}, duration);
-        })
-        .on('mouseout', function(){
-            $(this).find('strong, span').stop(true).animate({opacity: 0}, duration);
-        });
+      .on('mouseover', function() {
+        $(this).find('strong, span').stop(true).animate({
+          // strong = 文字 span = 古い感じの色
+          opacity: 1
+        }, duration)
+      })
+      .on('mouseout', function() {
+        $(this).find('strong, span').stop(true).animate({
+          opacity: 0
+        }, duration);
+      });
 
     // images 2つ目の画像
+    // $('#images p:nth-child(2)')でもOK
     $images.filter(':nth-child(2)')
-        .on('mouseover', function(){
-            $(this).find('strong').stop(true).animate({opacity: 1, left: '0%'}, duration);
-            $(this).find('span').stop(true).animate({opacity: 1}, duration);
-        })
-        .on('mouseout', function(){
-            $(this).find('strong').stop(true).animate({opacity: 0, left: '-200%'}, duration);
-            $(this).find('span').stop(true).animate({opacity: 0}, duration);
-        });
+      .on('mouseover', function() {
+        $(this).find('strong').stop(true).animate({
+          opacity: 1,
+          left: '0%'
+        }, duration);
+        $(this).find('span').stop(true).animate({
+          opacity: 1
+        }, duration);
+      })
+      .on('mouseout', function() {
+        $(this).find('strong').stop(true).animate({
+          opacity: 0,
+          left: '-200%'
+        }, duration);
+        $(this).find('span').stop(true).animate({
+          opacity: 0
+        }, duration);
+      });
 
     // images 3つ目の画像
     $images.filter(':nth-child(3)')
-        .on('mouseover', function(){
-            $(this).find('strong').stop(true).animate({bottom: '0px'}, duration);
-            $(this).find('span').stop(true).animate({opacity: 1}, duration);
-            $(this).find('img').stop(true).animate({top: '-20px'}, duration * 1.3);
-        })
-        .on('mouseout', function(){
-            $(this).find('strong').stop(true).animate({bottom: '-80px'}, duration);
-            $(this).find('span').stop(true).animate({opacity: 0}, duration);
-            $(this).find('img').stop(true).animate({top: '0px'}, duration);
-        });
+      .on('mouseover', function() {
+        $(this).find('strong').stop(true).animate({
+          bottom: '0px',
+          // opacity: 0.3
+        }, duration);
+        $(this).find('span').stop(true).animate({
+          opacity: 1
+        }, duration);
+        $(this).find('img').stop(true).animate({
+          top: '-20px'
+        }, duration * 1.3);
+      })
+      .on('mouseout', function() {
+        $(this).find('strong').stop(true).animate({
+          bottom: '-80px'
+        }, duration);
+        $(this).find('span').stop(true).animate({
+          opacity: 0
+        }, duration);
+        $(this).find('img').stop(true).animate( {
+          top: '0px'
+        }, duration);
+      });
+
 
 });
--- jQuery/新規/Chapter04/03/js/main.js
+++ jQuery/新規/Chapter04/03/js/main.js
@@ -1,29 +1,39 @@
 $(function(){
-    // 
+
     var duration = 300;
 
-    // buttons2 ----------------------------------------
-    $('#buttons2 button').each(function(index){
-        //var pos = Math.random() * 80 - 40;
-        var pos = index * 40 - 40;
-        $(this).css('top', pos);
+    $('#buttons2 button').each(function(index) {
+      var pos = index * 40 - 40;
+      $(this).css('top', pos);
     })
-    .on('mouseover', function(){
-        var $btn = $(this).stop(true).animate({
-            backgroundColor: '#faee00',
-            color: '#000'
-        }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 0}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 1}, duration);
+    // $('#buttons2 button')に対しての連続処理
+    .on('mouseover', function() {
+      var $btn = $(this).stop(true).animate({
+        backgroundColor: '#faee00',
+        color: '#000'
+      }, duration);
+      $btn.find('img:first-child').stop(true).animate({
+        opacity: 0
+      }, duration);
+      $btn.find('img:nth-child(2)').stop(true).animate({
+        opacity: 1
+      }, duration);
     })
-    .on('mouseout', function(){
-        var $btn = $(this).stop(true).animate({
-            backgroundColor: '#fff',
-            color: '#01b169',
-        }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 1}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 0}, duration);
+
+    .on('mouseout', function() {
+      var $btn = $(this).stop(true).animate({
+        backgroundColor: 'fff',
+        color: '#01b169',
+      }, duration);
+      // $(this).find('img:first-child').stop(true).animate({ でも良い？
+      $btn.find('img:first-child').stop(true).animate({
+        opacity: 1
+      }, duration);
+      $btn.find('img:nth-child(2)').stop(true).animate({
+        opacity: 0
+      }, duration)
     });
+
 
 
 });
